a813ce9e119be84340bede6212d8a7222a4881b5
Stephan <stephan.pichardo@gmail.com>
2025-10-15 15:55:25 -0400

    deleted:    fancy.html 	modified:   index.html 	new file:   js/getnav.js 	new file:   js/particles.js 	renamed:    js/nav.html -> nav.html 	modified:   style.css

---
+++ js/particles.js
@@ -0,0 +1,58 @@
+
+const canvas = document.getElementById('particle-bg');
+const ctx = canvas.getContext('2d');
+
+let particlesArray;
+let numParticles = 80;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 2 + 1;
+    this.speedX = (Math.random() - 0.5) * 0.3;
+    this.speedY = (Math.random() - 0.5) * 0.3;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    // wrap around edges
+    if (this.x < 0) this.x = canvas.width;
+    if (this.x > canvas.width) this.x = 0;
+    if (this.y < 0) this.y = canvas.height;
+    if (this.y > canvas.height) this.y = 0;
+  }
+  draw() {
+    ctx.fillStyle = 'rgba(157, 158, 165, 0.6)';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function init() {
+  particlesArray = [];
+  for (let i = 0; i < numParticles; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particlesArray.forEach(particle => {
+    particle.update();
+    particle.draw();
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
