2899769a385331f2a160859cd63f2dfc977f3296
Stephan <stephan.pichardo@gmail.com>
2025-10-23 16:40:33 -0400

    modified:   bi/projects/bludhaven.html 	new file:   bi/projects/content.json 	new file:   bi/projects/images/um.png 	new file:   bi/projects/js/get-content.js 	new file:   bi/projects/mermaid.txt 	modified:   style.css

---
+++ bi/projects/js/get-content.js
@@ -0,0 +1,54 @@
+
+fetch('content.json')
+  .then(response => response.json())
+  .then(content => {
+    const contentList = document.getElementById('content-list');
+    
+    content.forEach(item => {
+      const section = document.createElement('section');
+      section.innerHTML = `
+      <img src=${item.image} alt="UM" style="width:100%;height:100%;border-radius: 10px;">
+         <div style="text-align:center; font-weight:bold; line-height: 1; ">${item.title}</div>
+      `;
+
+      const modal = document.createElement('div');
+      modal.classList.add('modal');
+      modal.id = `modal-${item.name}`;
+
+      modal.innerHTML = `
+        <div class="modal-content">
+        <div style="text-align:center; font-weight:bold; line-height: 1; ">${item.title}</div>
+        </br>
+           <div class="looker-container" style="width:100%;height:100%;">
+      <iframe 
+        src="${item.link}"
+        loading="lazy"
+        frameborder="0"
+        style="border:none;width:100%;height:100%;">
+      </iframe>
+    </div>
+    </br>
+    <div>${item.summary}</div>
+        </div>
+      `;
+
+
+      contentList.appendChild(section);
+      document.body.appendChild(modal);
+
+ section.addEventListener('click', () => {
+        modal.style.display = 'block';
+      });
+
+      window.addEventListener('click', (event) => {
+        if (event.target == modal) {
+          modal.style.display = 'none';
+        }
+      });
+    });
+  })
+  .catch(error => {
+    console.error('Error loading content:', error);
+  });
+
+  
