c615239a6ffa6d781623403f61770697b035ea0e
Stephan Pichardo <stephan.pichardo@gmail.com>
2026-01-14 17:05:30 -0500

    new file:   bi/projects/tutor-testprep/getnav.js 	new file:   bi/projects/tutor-testprep/nav.html 	new file:   bi/projects/tutor-testprep/particles.js 	new file:   bi/projects/tutor-testprep/style.css 	modified:   bi/projects/tutor-testprep/tutor-testprep.html 	modified:   index.html

---
+++ bi/projects/tutor-testprep/getnav.js
@@ -0,0 +1,6 @@
+
+    fetch("nav.html")
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById("navbar").innerHTML = html;
+      });
---
+++ bi/projects/tutor-testprep/particles.js
@@ -0,0 +1,67 @@
+
+const canvas = document.getElementById('particle-bg');
+const ctx = canvas.getContext('2d');
+
+let particlesArray;
+let linesArray;
+let numParticles = 50;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+window.addEventListener('resize', resizeCanvas);
+resizeCanvas();
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 2 + 1;
+    this.speedX = (Math.random() - 0.5) * 0.3;
+    this.speedY = (Math.random() - 0.5) * 0.3;
+  }
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x < 0) this.x = canvas.width;
+    if (this.x > canvas.width) this.x = 0;
+    if (this.y < 0) this.y = canvas.height;
+    if (this.y > canvas.height) this.y = 0;
+  }
+  draw() {
+    ctx.fillStyle = 'rgba(179, 182, 198, 0.6)';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+  drawLines1() {
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(-100, -100);
+    ctx.closePath();
+    ctx.strokeStyle = 'rgba(106, 107, 109, 0.08)';
+    ctx.stroke();
+  }  
+}
+
+function init() {
+  particlesArray = [];
+  for (let i = 0; i < numParticles; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particlesArray.forEach(particle => {
+  particle.update();
+  particle.draw();
+  particle.drawLines1();
+  });
+  requestAnimationFrame(animate);
+}
+
+init();
+animate();
