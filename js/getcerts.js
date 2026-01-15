262403c3d9896e537ae1474c16749dfc0eba3cd9
Stephan Pichardo <stephan.pichardo@gmail.com>
2026-01-14 21:30:50 -0500

    modified:   js/getcerts.js 	modified:   json/projects.json

--- js/getcerts.js
+++ js/getcerts.js
@@ -1,4 +1,4 @@
-fetch('/pichardo-cv/certs.json')
+fetch('/pichardo-cv/json/certs.json')
   .then(response => response.json())
   .then(posts => {
     const certList = document.getElementById('cert-list');
